af3ec23892a71414a43175f3ae498c05d0843771
Toropov Dmitry <dimonsladkoed@mail.ru>
2020-03-21 18:38:43 +0300

    Task1

---
+++ IndexerTest(C++)/unittest1.cpp
@@ -0,0 +1,90 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../Task1(C++)/Indexer.h"
+#include "../Task1(C++)/Indexer.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace IndexerTest—
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		double* arr = new double[4]{ 1, 2, 3, 4 };
+		TEST_METHOD(HaveCorrectLength)
+		{
+			Indexer indexer = Indexer(arr, 4, 1, 2);
+			Assert::AreEqual(2, indexer.getLength());
+		}
+
+		TEST_METHOD(GetCorrectly)
+		{
+			Indexer indexer(arr, 4, 1, 2);
+			Assert::AreEqual(2.0, indexer[0]);
+			Assert::AreEqual(3.0, indexer[1]);
+		}
+
+		TEST_METHOD(SetCorrectly)
+		{
+			Indexer indexer(arr, 4, 1, 2);
+			indexer[0] = 10;
+			Assert::AreEqual(10.0, arr[1]);
+		}
+
+		TEST_METHOD(IndexerDoesNotCopyArray)
+		{
+			Indexer indexer1(arr, 4, 1, 2);
+			Indexer indexer2(arr, 4, 0, 2);
+			indexer1[0] = 100500;
+			Assert::AreEqual(100500.0, indexer2[1]);
+		}
+
+		TEST_METHOD(FailWithWrongArguments1)
+		{
+			auto func = []()
+			{
+				double* arr = new double[4]{ 1, 2, 3, 4 };
+				Indexer(arr, 4, -1, 3);
+			};
+			Assert::ExpectException<std::invalid_argument>(func);
+		}
+		TEST_METHOD(FailWithWrongArguments2)
+		{
+			auto func = []()
+			{
+				double* arr = new double[4]{ 1, 2, 3, 4 };
+				Indexer(arr, 4, 1, -1);
+			};
+			Assert::ExpectException<std::invalid_argument>(func);
+		}
+		TEST_METHOD(FailWithWrongArguments3)
+		{
+			auto func = []()
+			{
+				double* arr = new double[4]{ 1, 2, 3, 4 };
+				Indexer(arr, 4, 1, 10);
+			};
+			Assert::ExpectException<std::invalid_argument>(func);
+		}
+		TEST_METHOD(FailWithWrongIndexing1)
+		{
+			auto func = []()
+			{
+				double* arr = new double[4]{ 1, 2, 3, 4 };
+				Indexer indexer(arr, 4, 1, 2);
+				double tmp = indexer[-1];
+			};
+			Assert::ExpectException<std::invalid_argument>(func);
+		}
+		TEST_METHOD(FailWithWrongIndexing2)
+		{
+			auto func = []()
+			{
+				double* arr = new double[4]{ 1, 2, 3, 4 };
+				Indexer indexer(arr, 4, 1, 2);
+				double tmp = indexer[10];
+			};
+			Assert::ExpectException<std::invalid_argument>(func);
+		}
+	};
+}
